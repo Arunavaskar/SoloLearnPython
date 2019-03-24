58ee3d43a7dbf71b77b9263acb4b358e82cd977e
Arunava Chakraborty <arunavaskar@outlook.com>
2019-03-25 01:08:51 +0530

    Update Exception Handling.py

--- Exception Handling.py
+++ Exception Handling.py
@@ -18,8 +18,13 @@ except ZeroDivisionError: # except_ION
     # and wil return a desired output!
     print("Fuck, an error occured!")
     print(num3, " was num3 and ", num4, "was num4\nthey were devided by each other and caused a zero division error!")
+
+except:
+    print("an error occured")
+    raise
 # This code will run no matter what!
 # finally statement even runs if an uncaught exception occurs in one of the preceding block
+else:
+    print("congo there was no exceptions!")
 finally:
     print("You need to correct the code if it didn't run previously or if it did, congo")
-     
