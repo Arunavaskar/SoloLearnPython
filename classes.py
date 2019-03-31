daacd955da093870bc3ab317c9c045ceb00879aa
Arunava Chakraborty <arunavaskar@outlook.com>
2019-03-31 22:10:15 +0530

    Create classes.py

---
+++ classes.py
@@ -0,0 +1,7 @@
+class Multiplication:
+    def __init__(self, math):
+        self.math = math
+math = Multiplication(math = maths())             
+def maths():
+    i = int()
+    print(i , "answer")
