06fa60454d81144eaee7330dd62f652150325d38
Arunava Chakraborty <arunavaskar@outlook.com>
2019-04-01 23:15:42 +0530

    Update classes.py

--- classes.py
+++ classes.py
@@ -1,7 +1,11 @@
-class Multiplication:
-    def __init__(self, math):
-        self.math = math
-math = Multiplication(math = maths())             
-def maths():
-    i = int()
-    print(i , "answer")
+# python is a object oriented language
+# Almost everything in python is object orinted, with it's methods and properties 
+
+
+# creating a class
+
+class MyClass:
+    x = 5
+# create an object named p1, and print the value of x
+p1 = MyClass()
+print(p1.x)
